6bbf02d28df15ed1b1db3c4e1ab045032023f272
David Gonzalez <dave@bitshifter.co>
2021-11-06 15:40:40 -0400

    setup initial queries

---
+++ gql/recentRegistrationsQuery.ts
@@ -0,0 +1,37 @@
+import { gql } from '@apollo/client';
+import client from 'apollo-client';
+
+export enum OrderBy {
+  DESC = 'desc',
+  ASC = 'asc',
+}
+
+export const searchDomains = gql`
+  query searchDomains($first: Int, $where: Domain_filter) {
+    domains(first: $first, where: $where) {
+      id
+      name
+      labelName
+    }
+  }
+`;
+
+export const searchRegistrations = gql`
+  query getRegistrations($first: Int, $orderDirection: String) {
+    registrations(first: $first, orderDirection: $orderDirection, orderBy: registrationDate) {
+      domain {
+        name
+      }
+      id
+    }
+  }
+`;
+
+export const fetchRecentlyRegisteredDomains = async (first = 10, orderDirection = OrderBy.DESC) =>
+  client.query({
+    query: searchRegistrations,
+    variables: {
+      first,
+      orderDirection,
+    },
+  });
