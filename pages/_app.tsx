87c719b7fcaa1c697beb818566cff3ae55dddf6e
David Gonzalez <dave@bitshifter.co>
2021-11-06 15:41:27 -0400

    start developing initial page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
+import Head from 'next/head';
 import { ApolloProvider } from '@apollo/client';
-import Client from '../apollo-client';
-import '../styles/globals.css';
+import Client from 'apollo-client';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={Client}>
+      <Head>
+        <title>Search Registred ENS Domains</title>
+        <meta name='description' content='ENS Domain Search' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
       <Component {...pageProps} />
     </ApolloProvider>
   );
