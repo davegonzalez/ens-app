f56d2bd9da0d29a7ea9256a0d89dbeafa5565626
David Gonzalez <dave@bitshifter.co>
2021-11-11 16:11:10 -0500

    fix queries

--- gql/recentRegistrationsQuery.ts
+++ gql/recentRegistrationsQuery.ts
@@ -11,23 +11,31 @@ export const searchDomains = gql`
     domains(first: $first, where: $where) {
       id
       name
-      labelName
+      labelhash
     }
   }
 `;
 
 export const searchRegistrations = gql`
   query getRegistrations($first: Int, $orderDirection: String) {
-    registrations(first: $first, orderDirection: $orderDirection, orderBy: registrationDate) {
+    registrations(
+      first: $first
+      orderDirection: $orderDirection
+      orderBy: registrationDate
+    ) {
       domain {
         name
       }
       id
+      registrationDate
     }
   }
 `;
 
-export const fetchRecentlyRegisteredDomains = async (first = 10, orderDirection = OrderBy.DESC) =>
+export const fetchRecentlyRegisteredDomains = async (
+  first = 10,
+  orderDirection = OrderBy.DESC
+) =>
   client.query({
     query: searchRegistrations,
     variables: {
